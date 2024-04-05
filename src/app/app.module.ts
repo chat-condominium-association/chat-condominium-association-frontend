4949c19b550cd3e4effca138c027c2996b6f7660
Yelyzaveta <liza.vasilenko13@gmail.com>
2024-04-05 19:10:23 +0200

    chore: install ngrx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { AppComponent } from './app.component';
 import { CoreModule } from '@core/core.module';
 import { MatDialogRef } from '@angular/material/dialog';
 import { CommonModule } from '@angular/common';
+import { StoreModule } from '@ngrx/store';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [CommonModule, AppRoutingModule, CoreModule],
+  imports: [CommonModule, AppRoutingModule, CoreModule, StoreModule.forRoot({}, {})],
   bootstrap: [AppComponent],
   providers: [{ provide: MatDialogRef, useValue: {} }],
 })
