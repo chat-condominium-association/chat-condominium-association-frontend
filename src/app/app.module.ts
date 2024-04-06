c5d9ed7ba5080e5a7148d8fe3058bdb1ba11fc0e
Yelyzaveta <liza.vasilenko13@gmail.com>
2024-04-06 21:25:18 +0200

    chore: add some ngrx base folder structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from '@core/core.module';
 import { MatDialogRef } from '@angular/material/dialog';
 import { CommonModule } from '@angular/common';
 import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [CommonModule, AppRoutingModule, CoreModule, StoreModule.forRoot({}, {})],
+  imports: [
+    CommonModule,
+    AppRoutingModule,
+    CoreModule,
+    StoreModule.forRoot({}, {}),
+    EffectsModule.forRoot([]),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: !isDevMode(),
+      autoPause: true,
+    }),
+  ],
   bootstrap: [AppComponent],
   providers: [{ provide: MatDialogRef, useValue: {} }],
 })
---
+++ src/app/store/app.state.interface.ts
@@ -0,0 +1,10 @@
+export interface AppState {
+  appState: StoreState;
+}
+
+export interface StoreState {
+  admin: string;
+  user: string;
+}
+
+//messages?
