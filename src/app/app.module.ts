bbc8432bc274c56fcf17dfd0963c7f23577aa05c
Yelyzaveta <liza.vasilenko13@gmail.com>
2024-04-09 14:44:55 +0200

    refactor: move fonst declaration to the scss file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from '@core/core.module';
 import { MatDialogRef } from '@angular/material/dialog';
-import { CommonModule, HashLocationStrategy, LocationStrategy } from '@angular/common';
+import { CommonModule } from '@angular/common';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
@@ -23,9 +23,6 @@ import { StoreDevtoolsModule } from '@ngrx/store-devtools';
     }),
   ],
   bootstrap: [AppComponent],
-  providers: [
-    { provide: MatDialogRef, useValue: {} },
-    { provide: LocationStrategy, useClass: HashLocationStrategy },
-  ],
+  providers: [{ provide: MatDialogRef, useValue: {} }],
 })
 export class AppModule {}
