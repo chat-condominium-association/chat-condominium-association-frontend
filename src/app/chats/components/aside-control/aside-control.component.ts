ffe33c81550e619d82c0d1488f8d6538286ba88f
Yelyzaveta <liza.vasilenko13@gmail.com>
2024-04-23 22:37:35 +0200

    fix: fix scrollbar

--- src/app/chats/components/aside-control/aside-control.component.ts
+++ src/app/chats/components/aside-control/aside-control.component.ts
@@ -1,10 +1,10 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostBinding, OnDestroy, inject } from '@angular/core';
 import { Store, select } from '@ngrx/store';
 import { chats } from '@shared/data/chats.imges';
 import { AsidePanel } from '@shared/enums/aside-panel-states.enum';
 import { StoreState } from '@store/app.state.interface';
 import { asideStateSelector } from '@store/ui/components/components.selectors';
-import { Observable } from 'rxjs';
+import { Observable, Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-aside-control',
@@ -12,13 +12,24 @@ import { Observable } from 'rxjs';
   styleUrls: ['./aside-control.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AsideControlComponent {
+export class AsideControlComponent implements OnDestroy {
   readonly AsidePanel = AsidePanel;
   readonly ChatsImages = chats;
+  private destroy$ = new Subject<void>();
+
+  @HostBinding('class.hidden') isAsideHidden = false;
 
   private store = inject(Store<StoreState>);
   protected asideState$: Observable<AsidePanel>;
   constructor() {
     this.asideState$ = this.store.pipe(select(asideStateSelector));
+    this.asideState$.pipe(takeUntil(this.destroy$)).subscribe(state => {
+      this.isAsideHidden = state === AsidePanel.Hidden;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
